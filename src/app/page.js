d00e28526be1ae41ebfe03aa19330adb8c9cdc38
Watson <watsontavares72@gmail.com>
2023-09-21 02:14:48 +0100

    feature: back button, repos card, repos route core: repos update

--- src/app/page.js
+++ src/app/page.js
@@ -6,80 +6,54 @@ import "react-toastify/dist/ReactToastify.css";
 
 import Message from "@/components/Alert/Message";
 
-import Loading from "@/app/loading";
-
 import Link from "next/link";
 
 export default function Home() {
-  //benildebonfim
-  const [erroMessage, setErrorMessage] = useState("");
   const [user, setUser] = useState("");
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
 
   async function getRepo(username) {
     if (user === "") {
       Message("Error", "Insira um nome de utilizador!");
       return false;
     }
-    setIsLoading(true);
-    await fetch(`https://api.github.com/users/${username}/repos`).then(
-      async (data) => {
-        await data.json().then((res) => {
-          if (res.length === 0) {
-            Message("Error", "Repositório não encontrado!");
-            setIsLoading(false);
-          } else {
-            setData(res);
-            setIsLoading(false);
-            Message("Success", "Repositório encontrado!");
-          }
-        });
-      }
-    );
   }
 
   return (
     <>
       <ToastContainer />
-      <main className="flex flex-col items-center justify-center h-screen w-full bg-gradient-to-r from-indigo-300 to-fuchsia-200 sm:pt-20">
+      <main className="flex flex-col items-center justify-center h-screen  bg-gradient-to-r from-indigo-300 to-fuchsia-200 sm:pt-20">
         <Link href="#" className="absolute top-8 right-8">
           <AiFillGithub className="text-xl" />
         </Link>
-        {isLoading === true ? (
-          <Loading />
-        ) : (
-          <>
-            {data.length > 0 ? (
-              <ul>
-                {data.map((repo) => {
-                  return <li key={repo.id}>{repo.name}</li>;
-                })}
-              </ul>
+        <div className="flex flex-col items-center justify-center w-full">
+          <h1 className="text-5xl  text-[#1ED950] mb-12 md:mb-0">𝓜𝓲 𝓡𝓮𝓹𝓸𝓼</h1>
+          <div className="flex flex-col  items-center justify-center md:flex-row h-full w-full p-8 md:p-32">
+            <input
+              value={user || ""}
+              onChange={(event) => setUser(event.target.value)}
+              type="text"
+              placeholder="Insira o utilizador do GitHub"
+              className="p-3 w-full md:w-96 mb-5 md:mb-0 md:mr-3 outline-none border-[1px] border-indigo-700 focus:drop-shadow-md text-zinc-700 rounded-sm transition-all focus:placeholder:bottom-8 focus:placeholder:text-xs"
+            />
+            {user.length > 0 ? (
+              <Link
+                className="flex items-center justify-center py-3 md:px-8 w-full md:w-auto outline-none bg-indigo-700 hover:bg-indigo-600 transition-all text-white rounded-sm"
+                onClick={() => getRepo(user)}
+                href={`/repositorios/${user}`}
+              >
+                Procurar
+              </Link>
             ) : (
-              <>
-                <h1 className="text-5xl mb-12 sm:mb-0 sm:mt-18 text-[#1ED950]">
-                  𝓜𝓲 𝓡𝓮𝓹𝓸𝓼
-                </h1>
-                <div className="flex flex-col  items-center justify-center md:flex-row w-full p-4 sm:p-32">
-                  <input
-                    value={user || ""}
-                    onChange={(event) => setUser(event.target.value)}
-                    type="text"
-                    placeholder="Insira o utilizador do GitHub"
-                    className="p-3 w-full md:w-96 mb-4 md:mb-0 md:mr-3 outline-none border-[1px] border-indigo-700 focus:drop-shadow-md text-zinc-700 rounded-sm transition-all focus:placeholder:bottom-8 focus:placeholder:text-xs"
-                  />
-                  <button
-                    className="py-3 md:px-8 w-full md:w-auto outline-none bg-indigo-700 hover:bg-indigo-600 transition-all text-white rounded-sm"
-                    onClick={() => getRepo(user)}
-                  >
-                    Procurar
-                  </button>
-                </div>
-              </>
+              <Link
+                className="flex items-center justify-center py-3 md:px-8 w-full md:w-auto outline-none bg-indigo-700 hover:bg-indigo-600 transition-all text-white rounded-sm"
+                onClick={() => getRepo(user)}
+                href={""}
+              >
+                Procurar
+              </Link>
             )}
-          </>
-        )}
+          </div>
+        </div>
       </main>
     </>
   );
---
+++ src/app/repositorios/[userName]/page.js
@@ -0,0 +1,110 @@
+"use client";
+import { useState, useEffect } from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { TbArrowBackUp } from "react-icons/tb";
+import { ToastContainer } from "react-toastify";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import "react-toastify/dist/ReactToastify.css";
+
+import Message from "@/components/Alert/Message";
+
+import Loading from "@/app/loading";
+
+import Link from "next/link";
+
+export default function Repositories({ params }) {
+  //benildebonfim
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  async function getRepo(username) {
+    setIsLoading(true);
+    await fetch(`https://api.github.com/users/${username}/repos`).then(
+      async (data) => {
+        await data.json().then((res) => {
+          if (res.length === 0) {
+            Message("Error", "Repositório não encontrado!");
+            setIsLoading(false);
+          } else {
+            console.log(res);
+            setData(res);
+            setIsLoading(false);
+            Message("Success", "Repositório encontrado!");
+          }
+        });
+      }
+    );
+  }
+
+  useEffect(() => {
+    getRepo(params.userName);
+  }, [params.userName]);
+  return (
+    <>
+      <ToastContainer />
+      <main className="flex flex-col items-center justify-center min-h-screen w-full  bg-gradient-to-r from-indigo-300 to-fuchsia-200 sm:pt-20">
+        <Link href="#" className="absolute top-8 right-8">
+          <AiFillGithub className="text-xl" />
+        </Link>
+        <button
+          className="absolute top-6 left-8"
+          onClick={() => {
+            router.back();
+          }}
+        >
+          <TbArrowBackUp className="text-3xl text-red-600 transition-all hover:text-red-500"/>
+        </button>
+        {isLoading === true ? (
+          <Loading />
+        ) : data.length > 0 ? (
+          <>
+            <div className="flex mt-24 md:mt-0 items-center justify-center rounded-full h-32 w-32 bg-indigo-700">
+              <Image
+                height={120}
+                width={120}
+                alt="user image"
+                src={`${data[0].owner.avatar_url}`}
+                className="rounded-full"
+              />
+            </div>
+            <small className="text-zinc-700 mt-3 text-lg">
+              {data[0].owner.login} 🚀
+            </small>
+            <ul className="flex flex-wrap items-center justify-center p-8 sm:p-12 lg:p-16 w-full h-full ">
+              {data.map((repo) => {
+                return (
+                  <li
+                    className="text-white mr-4 p-8 mb-4 h-36 w-full  md:w-64 text-lg
+                  flex flex-col items-center cursor-pointer shadow-md 
+                  justify-center transition-all overflow-hidden
+                   bg-indigo-700 hover:bg-indigo-600 font-medium
+                  hover:shadow-xl hover:shadow-indigo-950 relative"
+                    key={repo.id}
+                  >
+                    <span className="absolute top-2 right-2">📌</span>
+                    {repo.name}
+                    <span className="ml-auto text-[10px] text-zinc-300 absolute right-2 bottom-2">
+                      {repo.language}
+                    </span>
+                    <span className="ml-auto text-[10px] text-zinc-300 absolute left-2 bottom-2">
+                      {repo.updated_at.slice(0, 10)}
+                    </span>
+                  </li>
+                );
+              })}
+            </ul>
+          </>
+        ) : (
+          <div className="flex flex-col items-center justify-center">
+            <span className="text-7xl mb-4">😔</span>
+            <h1 className="font-bold text-3xl mb-5 mt-4 text-gray-700 text-center">
+              Repositório não encontrado!
+            </h1>
+          </div>
+        )}
+      </main>
+    </>
+  );
+}
